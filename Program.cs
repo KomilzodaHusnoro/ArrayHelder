1a91a94e1f95b21df8a1d02e80f149084e4e5df7
Husnoro <Husnoro>
2020-04-08 12:48:59 +0500

    незавершенный вариант

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+        int [] numbers = {1,2,3,4,5};
+        new ArrayHelder.Pow();
+        
+        }
+    }
+    static class ArrayHelder
+    {
+        static string Pow( ref string [] stringArr )
+        {
+            return stringArr[stringArr.Length -1];
+        }
+        static int Push( ref int [] intArr )
+        {
+            return intArr[intArr.Length+1];
+    
+        }
+        static  double Shift( ref double [] doubleArr)
+        {
+
+        }
+        static float UnShift( ref float [] floatArr )
+        {
+
+        }
+    }
+}
